641df1d6486794b77ba053d2228ca5c8ce9a75c6
nandirchrd <nandirchrd@gmail.com>
2022-03-27 15:55:32 +0700

    deleted and updated some files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './app';
+import './styles/bootstrap.min.css';
 
 render(<App />, document.getElementById('root'));
