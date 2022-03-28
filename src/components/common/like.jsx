516db9b6c38a33b85184ab11f78eda8eace201bb
nandirchrd <nandirchrd@gmail.com>
2022-03-29 01:52:17 +0700

    added react-icons dependency and added some icons

---
+++ src/components/common/like.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+
+const Like = (props) => {
+  const { liked, onClick } = props;
+  return liked ? (
+    <FaHeart
+      onClick={onClick}
+      style={{ cursor: 'pointer', color: '#DC3545' }}
+    />
+  ) : (
+    <FaRegHeart
+      onClick={onClick}
+      style={{ cursor: 'pointer', color: '#DC3545' }}
+    />
+  );
+};
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { getMovies } from './../services/fakeMovieService';
+import { FaTrash } from 'react-icons/fa';
+import Like from './common/like';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
   };
+  componentDidUpdate() {
+    console.log(this.state.movies);
+  }
   handleDelete = (movie) => {
     const { movies } = this.state;
     const moviesFiltered = movies.filter((m) => m !== movie);
     this.setState({ movies: moviesFiltered });
+  };
+  handleLike = (movie) => {
+    const { movies } = this.state;
+    const index = movies.indexOf(movie);
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
   render() {
     const { movies } = this.state;
@@ -27,20 +38,27 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             {movies.map((movie) => (
-              <tr key={movie.id}>
+              <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
                     className='btn btn-danger btn-sm'>
-                    Delete
+                    <FaTrash />
                   </button>
                 </td>
               </tr>
